781825194012475b6eb50f9fcdb3f5c705a9974b
Carlos3888 <110266967+Carlos3888@users.noreply.github.com>
2022-09-02 14:29:12 -0200

    Creacion de las clases plantas y zombies

---
+++ src/lab7p2_carlosmurillo/Plantas.java
@@ -0,0 +1,140 @@
+package lab7p2_carlosmurillo;
+
+public class Plantas {
+    private String tipo;
+    
+    private String nombre;
+    private int ataque;
+    private int vida;
+    private String rango;
+    
+    private int explsion;
+    
+    private String proyectil;
+    private String color;
+    
+    private int altura;
+    private int peso;
+    private int dureza;
+
+    public Plantas() {
+    }
+
+    public Plantas(String tipo, String nombre, int ataque, int vida, String rango, int explsion) {
+        this.tipo = tipo;
+        this.nombre = nombre;
+        this.ataque = ataque;
+        this.vida = vida;
+        this.rango = rango;
+        this.explsion = explsion;
+    }
+
+    public Plantas(String tipo, String nombre, int ataque, int vida, String rango, String proyectil, String color) {
+        this.tipo = tipo;
+        this.nombre = nombre;
+        this.ataque = ataque;
+        this.vida = vida;
+        this.rango = rango;
+        this.proyectil = proyectil;
+        this.color = color;
+    }
+
+    public Plantas(String tipo, String nombre, int ataque, int vida, String rango, int altura, int peso, int dureza) {
+        this.tipo = tipo;
+        this.nombre = nombre;
+        this.ataque = ataque;
+        this.vida = vida;
+        this.rango = rango;
+        this.altura = altura;
+        this.peso = peso;
+        this.dureza = dureza;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getAtaque() {
+        return ataque;
+    }
+
+    public void setAtaque(int ataque) {
+        this.ataque = ataque;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+
+    public String getRango() {
+        return rango;
+    }
+
+    public void setRango(String rango) {
+        this.rango = rango;
+    }
+
+    public int getExplsion() {
+        return explsion;
+    }
+
+    public void setExplsion(int explsion) {
+        this.explsion = explsion;
+    }
+
+    public String getProyectil() {
+        return proyectil;
+    }
+
+    public void setProyectil(String proyectil) {
+        this.proyectil = proyectil;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+
+    public int getDureza() {
+        return dureza;
+    }
+
+    public void setDureza(int dureza) {
+        this.dureza = dureza;
+    }
+
+    @Override
+    public String toString() {
+        return "Plantas{" + "nombre=" + nombre + ", ataque=" + ataque + ", vida=" + vida + ", rango=" + rango + ", explsion=" + explsion + ", proyectil=" + proyectil + ", color=" + color + ", altura=" + altura + ", peso=" + peso + ", dureza=" + dureza + '}';
+    }
+    
+    
+    
+}
---
+++ src/lab7p2_carlosmurillo/Zombies.java
@@ -0,0 +1,137 @@
+package lab7p2_carlosmurillo;
+
+public class Zombies {
+    private String tipo;
+    
+    private String nombre;
+    private int ataque;
+    private int vida;
+    
+    private int experiencia;
+    private String bandera;
+    private String direccion;
+    
+    private int tamano;
+    private int edad;
+    private int enojo;
+    private String personas;
+
+    public Zombies() {
+    }
+
+    public Zombies(String tipo, String nombre, int ataque, int vida, int experiencia, String bandera, String direccion) {
+        this.tipo = tipo;
+        this.nombre = nombre;
+        this.ataque = ataque;
+        this.vida = vida;
+        this.experiencia = experiencia;
+        this.bandera = bandera;
+        this.direccion = direccion;
+    }
+
+    public Zombies(String tipo, String nombre, int ataque, int vida, int tamano, int edad, int enojo, String personas) {
+        this.tipo = tipo;
+        this.nombre = nombre;
+        this.ataque = ataque;
+        this.vida = vida;
+        this.tamano = tamano;
+        this.edad = edad;
+        this.enojo = enojo;
+        this.personas = personas;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getAtaque() {
+        return ataque;
+    }
+
+    public void setAtaque(int ataque) {
+        this.ataque = ataque;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+
+    public int getExperiencia() {
+        return experiencia;
+    }
+
+    public void setExperiencia(int experiencia) {
+        this.experiencia = experiencia;
+    }
+
+    public String getBandera() {
+        return bandera;
+    }
+
+    public void setBandera(String bandera) {
+        this.bandera = bandera;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public int getTamano() {
+        return tamano;
+    }
+
+    public void setTamano(int tamano) {
+        this.tamano = tamano;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getEnojo() {
+        return enojo;
+    }
+
+    public void setEnojo(int enojo) {
+        this.enojo = enojo;
+    }
+
+    public String getPersonas() {
+        return personas;
+    }
+
+    public void setPersonas(String personas) {
+        this.personas = personas;
+    }
+
+    @Override
+    public String toString() {
+        return "Zombies{" + "tipo=" + tipo + ", nombre=" + nombre + ", ataque=" + ataque + ", vida=" + vida + ", experiencia=" + experiencia + ", bandera=" + bandera + ", direccion=" + direccion + ", tamano=" + tamano + ", edad=" + edad + ", enojo=" + enojo + ", personas=" + personas + '}';
+    }
+    
+    
+}
